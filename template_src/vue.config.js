08d349b5abb2371f80799cdf11f67ed920ecb053
Alvis Zhao <alvisisme@163.com>
2020-10-10 09:18:06 +0800

    build(webpack): 加入构建时间显示

--- template_src/vue.config.js
+++ template_src/vue.config.js
@@ -1,14 +1,27 @@
 const path = require('path')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const chalk = require('chalk')
+
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const smp = new SpeedMeasurePlugin()
 
 module.exports = {
   publicPath: '',
   outputDir: 'www',
   pluginOptions: {
     'style-resources-loader': {
-        preProcessor: 'less',
-        patterns: [
-          path.resolve(__dirname, './src/styles/variable.less')
-         ]
-     }
- }
+      preProcessor: 'less',
+      patterns: [
+        path.resolve(__dirname, './src/styles/variable.less')
+      ]
+    }
+  },
+  configureWebpack: smp.wrap({
+    plugins: [
+      new ProgressBarPlugin({
+        format: ' build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
+        clear: false
+      })
+    ]
+  })
 }
