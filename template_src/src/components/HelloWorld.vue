0f2e634ce2dcbab58e739cc76bc8c88d8c6e9827
Alvis Zhao <alvisisme@163.com>
2020-10-16 20:53:42 +0800

    feat(eslint): 启用新eslint规则

--- template_src/src/components/HelloWorld.vue
+++ template_src/src/components/HelloWorld.vue
@@ -2,11 +2,14 @@
   <div class="HelloWorld">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
+      For a guide and recipes on how to configure / customize this project,<br>
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener" class="c-HelloWorld-link"
-        >vue-cli documentation</a
-      >.
+      <a
+        href="https://cli.vuejs.org"
+        target="_blank"
+        rel="noopener"
+        class="c-HelloWorld-link"
+      >vue-cli documentation</a>.
     </p>
     <h3 class="c-HelloWorld-item">Installed CLI Plugins</h3>
     <ul class="c-HelloWorld-list">
@@ -16,8 +19,7 @@
           target="_blank"
           rel="noopener"
           class="c-HelloWorld-link"
-          >babel</a
-        >
+        >babel</a>
       </li>
       <li class="c-HelloWorld-listItem">
         <a
@@ -25,8 +27,7 @@
           target="_blank"
           rel="noopener"
           class="c-HelloWorld-link"
-          >router</a
-        >
+        >router</a>
       </li>
       <li class="c-HelloWorld-listItem">
         <a
@@ -34,8 +35,7 @@
           target="_blank"
           rel="noopener"
           class="c-HelloWorld-link"
-          >vuex</a
-        >
+        >vuex</a>
       </li>
       <li class="c-HelloWorld-listItem">
         <a
@@ -43,8 +43,7 @@
           target="_blank"
           rel="noopener"
           class="c-HelloWorld-link"
-          >eslint</a
-        >
+        >eslint</a>
       </li>
       <li class="c-HelloWorld-listItem">
         <a
@@ -52,43 +51,69 @@
           target="_blank"
           rel="noopener"
           class="c-HelloWorld-link"
-          >unit-jest</a
-        >
+        >unit-jest</a>
       </li>
     </ul>
     <h3>Essential Links</h3>
     <ul class="c-HelloWorld-list">
       <li class="c-HelloWorld-listItem">
-        <a href="https://vuejs.org" target="_blank" rel="noopener" class="c-HelloWorld-link">Core Docs</a>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+          rel="noopener"
+          class="c-HelloWorld-link"
+        >Core Docs</a>
       </li>
       <li class="c-HelloWorld-listItem">
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener" class="c-HelloWorld-link"
-          >Forum</a
-        >
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+          rel="noopener"
+          class="c-HelloWorld-link"
+        >Forum</a>
       </li>
       <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener" class="c-HelloWorld-link"
-          >Community Chat</a
-        >
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+          rel="noopener"
+          class="c-HelloWorld-link"
+        >Community Chat</a>
       </li>
       <li class="c-HelloWorld-listItem">
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener" class="c-HelloWorld-link"
-          >Twitter</a
-        >
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+          rel="noopener"
+          class="c-HelloWorld-link"
+        >Twitter</a>
       </li>
       <li class="c-HelloWorld-listItem">
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener" class="c-HelloWorld-link">News</a>
+        <a
+          href="https://news.vuejs.org"
+          target="_blank"
+          rel="noopener"
+          class="c-HelloWorld-link"
+        >News</a>
       </li>
     </ul>
     <h3>Ecosystem</h3>
     <ul class="c-HelloWorld-list">
       <li class="c-HelloWorld-listItem">
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener" class="c-HelloWorld-link"
-          >vue-router</a
-        >
+        <a
+          href="https://router.vuejs.org"
+          target="_blank"
+          rel="noopener"
+          class="c-HelloWorld-link"
+        >vue-router</a>
       </li>
       <li class="c-HelloWorld-listItem">
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener" class="c-HelloWorld-link">vuex</a>
+        <a
+          href="https://vuex.vuejs.org"
+          target="_blank"
+          rel="noopener"
+          class="c-HelloWorld-link"
+        >vuex</a>
       </li>
       <li class="c-HelloWorld-listItem">
         <a
@@ -96,13 +121,15 @@
           target="_blank"
           rel="noopener"
           class="c-HelloWorld-link"
-          >vue-devtools</a
-        >
+        >vue-devtools</a>
       </li>
       <li class="c-HelloWorld-listItem">
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener" class="c-HelloWorld-link"
-          >vue-loader</a
-        >
+        <a
+          href="https://vue-loader.vuejs.org"
+          target="_blank"
+          rel="noopener"
+          class="c-HelloWorld-link"
+        >vue-loader</a>
       </li>
       <li class="c-HelloWorld-listItem">
         <a
@@ -110,8 +137,7 @@
           target="_blank"
           rel="noopener"
           class="c-HelloWorld-link"
-          >awesome-vue</a
-        >
+        >awesome-vue</a>
       </li>
     </ul>
   </div>
@@ -121,7 +147,10 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: {
+      type: String,
+      default: ''
+    }
   }
 }
 </script>
